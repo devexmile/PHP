de151fd7b17ed5563238e5b2b040a588a36d4bd6
Kevin <kevinpost16@gmail.com>
2019-06-20 20:32:21 -0400

    Wording update

--- Object_Oriented_PHP/Laracasts/task.php
+++ Object_Oriented_PHP/Laracasts/task.php
@@ -15,8 +15,8 @@ class Task {
   public $completed = false;
 
   // Here I'm adding a special method to the class.  All functions inside of a class are classed methods on an object.
-  // This method is also what is called a constructor.  A constructor is called imiditly when a class is instaneated. 
-  // - If arguments are made available/given when the class is instateated, they will be passed to the constructor when fired. 
+  // This method is also what is called a constructor.  A constructor is called right when a class is instantiated. 
+  // - If arguments are made available/given when the class is instantiated, they will be passed to the constructor when fired. 
   public function __construct($title, $description)
   {
     // An example to ensure that the constructor is firing when called "php task.php".  This is without having any arguments given.
@@ -40,7 +40,7 @@ class Task {
   }
 }
 
-// Now in order to get access to the class we have to create an instance of the class. The instances of the class is called an object.
+// Now in order to get access to the class we have to create an instance of the class. The instance of the class is called an object.
 // - The variable can be called anything = $task
 // - The = new means that we are creating a new object
 // - We have to identify the class that we are creating a new object from = Task();
@@ -49,19 +49,19 @@ class Task {
 // This is how you would pass arguments and the constructor pick them up.
 // $task = new Task('Hello People');
 
-// Now you can have multiple instances of a class you just have to ensure you name them differently.
+// Now you can have multiple instance of a class you just have to ensure you name them differently.
 // $task2 = new Task('Work this MAN!');
 
-// Now I'm going to have an instances of a class that will return two arguments ($title, $description). 
+// Now I'm going to have an instance of a class that will return two arguments ($title, $description). 
 $task3 = new Task('Kevin', 'Work this MAN!');
 
 // By do the above we now have access to the description property.  We're going to var_dump the information for the description property.  If the description property hasn't been set then you will receive a NULL value in the return.
 // var_dump($task->description);
 
-// Now in order to call the second instances of this class you have to update you information.
+// Now in order to call the second instance of this class you have to update you information.
 // var_dump($task2->description);
 
-// So here I'm running a var_dump on the property above.  You must have an instances of the class in order for this to work.
+// So here I'm running a var_dump on the property above.  You must have an instance of the class in order for this to work.
 // var_dump($task->completed);
 
 // The following is referring to a method and not a property.  Again methods are used as a term for a function.
